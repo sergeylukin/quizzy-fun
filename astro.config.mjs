0ec138ccdec78ce8d0ca582e50306ed3e9647c64
Sergey Lukin <contact@sergeylukin.com>
2024-05-18 13:02:26 +0300

    install alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import icon from "astro-icon";
-
 import htmx from "astro-htmx";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false
-  }), compress(), htmx()]
+  }), compress(), htmx(), alpinejs()]
 });
