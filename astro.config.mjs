9d51f89a7abdb965fa767ca6ebd765097c597473
Sergey Lukin <contact@sergeylukin.com>
2024-05-20 14:47:17 +0300

    formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
-import icon from 'astro-icon'
-import htmx from 'astro-htmx'
-import alpinejs from '@astrojs/alpinejs'
-import liciousI18n from '@astrolicious/i18n'
-const defaultLocale = 'en'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import icon from 'astro-icon';
+import htmx from 'astro-htmx';
+import alpinejs from '@astrojs/alpinejs';
+import liciousI18n from '@astrolicious/i18n';
+const defaultLocale = 'en';
 // const locales = {
 //   en: 'en-US',
 //   // the `defaultLocale` value must present in `locales` keys
 //   ru: 'ru-RU',
 // }
-const locales = ['en', 'ru']
+const locales = ['en', 'ru'];
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,19 +22,10 @@ export default defineConfig({
   // build: {
   //   format: 'file'
   // },
-  integrations: [
-    mdx(),
-    icon(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    compress(),
-    htmx(),
-    alpinejs(),
-    liciousI18n({
-      defaultLocale,
-      locales,
-    }),
-  ],
-})
-
+  integrations: [mdx(), icon(), tailwind({
+    applyBaseStyles: false
+  }), compress(), htmx(), alpinejs(), liciousI18n({
+    defaultLocale,
+    locales
+  })]
+});
